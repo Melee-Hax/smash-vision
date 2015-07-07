2b7ac400e42c794fe39c72372da58ccaa317f5e9
Peter Davids <peterdavids@PMBP.local>
2015-07-06 21:48:04 -0500

    baby steps towards reading the timer

--- smashProj/main.cpp
+++ smashProj/main.cpp
@@ -1,4 +1,8 @@
 #include "ObjectTracker.h"
+<<<<<<< HEAD
+#include <list>
+=======
+>>>>>>> a841d0afdc6bacf5d2ed2eec03b8e8f750421b3b
 
 /*
  ********FUNCTIONS ARE REMNANTS OF THE KEYPOINT MATCHING METHOD. MAY NEED TO FALL BACK ON THIS FOR SCALE INVARIANT MATCHING , SO LEAVING IN********
@@ -11,6 +15,20 @@ std::vector< cv::DMatch > match( cv::Mat descriptors_object , cv::Mat descriptor
 
 cv::Mat localize( cv::Mat img_object , std::vector<cv::KeyPoint> keypoints_object , cv::Mat img_scene , std::vector<cv::KeyPoint> keypoints_scene , std::vector< cv::DMatch > matches, bool debug = false);
 */
+<<<<<<< HEAD
+int errorFlag = 0;                  //error flag
+const int STARTFRAME = 250;         //what frame should we start on?
+float avgDistance;                  //should we find a match, how good is it?
+float THRESHOLD = .3;         //minimum acceptable template match accuracy
+
+std::pair<double,cv::Point>* templateMatch(cv::Mat img, cv::Mat templ, bool relaxed = false, float thresh = THRESHOLD);
+void getScale(cv::Mat);
+int readCountdown(cv::Mat scene);
+int getCurrentTime(cv::Mat scene);
+void setUpNumbers(cv::Mat scene);
+bool compPoints (cv::Point i,cv::Point j);
+
+=======
 std::pair<double,cv::Point>* templateMatch(cv::Mat img, cv::Mat templ, bool relaxed = false);
 float getScale(cv::Mat);
 int readCountdown(cv::Mat scene);
@@ -20,12 +38,35 @@ const int STARTFRAME = 0;         //what frame should we start on?
 float avgDistance;                  //should we find a match, how good is it?
 float THRESHOLD = .3;         //minimum acceptable template match accuracy
 
+>>>>>>> a841d0afdc6bacf5d2ed2eec03b8e8f750421b3b
 float videoScale;
 float game_start_time;
 
 using namespace cv;
 using namespace std;
 
+<<<<<<< HEAD
+Mat Zero;
+Mat One;
+Mat Two;
+Mat Three;
+Mat Four;
+Mat Five;
+Mat Six;
+Mat Seven;
+Mat Eight;
+Mat Nine;
+
+int main(int argc, char **argv)
+{
+    std::string filename = "/Users/peterdavids/Desktop/Projects/smashProj/smashProj/Resources/match.mp4";
+    
+    vision::ObjectTracker tracker(filename);            //init tracker, will give us img_scene values
+    
+    Mat img_object = imread("/Users/peterdavids/Desktop/Projects/smashProj/smashProj/Resources/pikachu.png",1);
+    Mat img_scene = tracker.getCurrentFrame();      //img_scene points to first frame
+    
+=======
 int main(int argc, char **argv)
 {
     std::string filename = "/Users/klaw/Documents/melee_hax/smash-vision/smashProj/Resources/match.mp4";
@@ -39,10 +80,17 @@ int main(int argc, char **argv)
     //std::vector<cv::Mat> ch;
     //split(img_object, ch);                              //set up mask to pass to detector
     
+>>>>>>> a841d0afdc6bacf5d2ed2eec03b8e8f750421b3b
     while(tracker.getCurrentFrameNumber() < STARTFRAME){//loop until you get to the frame you're interested in
         tracker.nextFrame();
     }
     
+<<<<<<< HEAD
+    setUpNumbers(img_scene);//can't set up numbers before game screen loaded
+    //HOW DO WE FIGURE OUT WHEN THE GAME SCREEN IS UP?
+    
+=======
+>>>>>>> a841d0afdc6bacf5d2ed2eec03b8e8f750421b3b
     namedWindow("Frame " + to_string(tracker.getCurrentFrameNumber()));
     
     pair<double,Point> matchPoint;
@@ -60,10 +108,15 @@ int main(int argc, char **argv)
             imshow( "Match: " + to_string((1-matchPoint.first)*100) + "% certainty", img_scene );
             waitKey(0);
             destroyWindow("Match: "+ to_string((1-matchPoint.first)*100) + "% certainty" );
+<<<<<<< HEAD
+        }
+        std::cout << "Next frame...(" << tracker.getCurrentFrameNumber() << ")..." << std::endl;
+=======
             
             //use tracker.getCurrentTime and reading the first frame that time != 08:00 to try and figure out what time the game starts at (can't use pre-game countdown because it is a total lie (nice job smash developers))
         }
         std::cout << "Next frame...(" << tracker.getCurrentFrameNumber() << ")" << std::endl;
+>>>>>>> a841d0afdc6bacf5d2ed2eec03b8e8f750421b3b
         tracker.nextFrame();
         img_scene = tracker.getCurrentFrame();
         continue;
@@ -333,7 +386,11 @@ cv::Mat localize( cv::Mat img_object , std::vector<cv::KeyPoint> keypoints_objec
     return img_matches;
 }
 */
+<<<<<<< HEAD
+pair<double,Point>* templateMatch(cv::Mat img, cv::Mat templ, bool relaxed, float thresh)
+=======
 pair<double,Point>* templateMatch(cv::Mat img, cv::Mat templ, bool relaxed)
+>>>>>>> a841d0afdc6bacf5d2ed2eec03b8e8f750421b3b
 {
     using namespace std;
     using namespace cv;
@@ -363,7 +420,11 @@ pair<double,Point>* templateMatch(cv::Mat img, cv::Mat templ, bool relaxed)
     
     minVal = abs(minVal); //am i losing important info here?
     
+<<<<<<< HEAD
+    if(!relaxed && (minVal > thresh)){//returns 0,null if threshold violated
+=======
     if(!relaxed && (minVal > THRESHOLD)){//returns 0,null if threshold violated
+>>>>>>> a841d0afdc6bacf5d2ed2eec03b8e8f750421b3b
         return new pair<double,Point>;
     }
     
@@ -378,23 +439,219 @@ pair<double,Point>* templateMatch(cv::Mat img, cv::Mat templ, bool relaxed)
  This function is only necessary if different input videos have the game window at different sizes. I have no idea if this happens. If we find later that it does, we can use this function to figure out what to scale everything by (only relevant to reading the HUD, since characters are going to change scale regardless of window size)
 */
 
+<<<<<<< HEAD
+void getScale(cv::Mat scene)
+=======
 float getScale(cv::Mat scene)
+>>>>>>> a841d0afdc6bacf5d2ed2eec03b8e8f750421b3b
 {
     Mat scaleR = imread("/Users/peterdavids/Desktop/Projects/smashProj/smashProj/Resources/scaleR.png",1),resizedScaleR;
     Size size = scaleR.size();
     double scaleMatch,bestScaleMatch = 100;
     float bestScale = 0;
+<<<<<<< HEAD
+    pair<double,Point>* matchPoint;
+    for(float i=.5 ; i <= 2 ; i = i+.05)
+    {
+        resize(scaleR, resizedScaleR, cvSize(size.width * i, size.height * i));
+        matchPoint = templateMatch(scene,resizedScaleR,true);
+        scaleMatch = matchPoint->first;
+        if((scaleMatch < bestScaleMatch) && scaleMatch > 0)
+        {
+=======
     for(float i=.5 ; i <= 2 ; i = i+.05)
     {
         resize(scaleR, resizedScaleR, cvSize(size.width * i, size.height * i));
         scaleMatch = templateMatch(scene,resizedScaleR,true)->first;
         if((scaleMatch < bestScaleMatch) && scaleMatch > 0){
+>>>>>>> a841d0afdc6bacf5d2ed2eec03b8e8f750421b3b
             bestScaleMatch = scaleMatch;
             bestScale = i;
         }
     }
     
+<<<<<<< HEAD
+    videoScale = bestScale;
+}
+
+
+
+//this just doesn't work//
+int getCurrentTime(cv::Mat scene) //good lord this is awful
+{
+    Mat garb_scene;
+    scene.copyTo(garb_scene);
+    
+    Mat num;
+    
+    pair<double,Point>* matchPoint;
+    
+    list<Point> numbers;
+    int littleNums = 0;
+    int yLoc = 0;
+    
+    for(int i=0 ; i <= 9 ; i++)
+    {
+        switch (i) {
+            case 0:
+                num = Zero;
+                break;
+            case 1:
+                num = One;
+                break;
+            case 2:
+                num = Two;
+                break;
+            case 3:
+                num = Three;
+                break;
+            case 4:
+                num = Four;
+                break;
+            case 5:
+                num = Five;
+                break;
+            case 6:
+                num = Six;
+                break;
+            case 7:
+                num = Seven;
+                break;
+            case 8:
+                num = Eight;
+                break;
+            case 9:
+                num = Nine;
+                break;
+        }
+        
+        Size size = num.size();
+        if(littleNums<2)
+        {
+            matchPoint = templateMatch(garb_scene, num, false, .05);
+            if(matchPoint->first > 0){
+                rectangle(garb_scene, matchPoint->second, Point( matchPoint->second.x + num.cols , matchPoint->second.y + num.rows ), Scalar::all(0), CV_FILLED, 8, 0 );
+                //imshow("little " + to_string(i) + " " + to_string(matchPoint->first),garb_scene);
+                //waitKey(0);
+                numbers.push_back(*new Point(i,matchPoint->second.x));
+                if(numbers.size() == 6){
+                    i = 10;
+                    continue;
+                }
+                littleNums++;
+                i = -1;
+                continue;
+            }
+        }
+        resize(num, num, cvSize(size.height * 1.3333, size.width * 1.3333));
+        matchPoint = templateMatch(garb_scene, num, false, .07);
+        if(matchPoint->first > 0){
+            if(yLoc > 0 && (abs(yLoc - matchPoint->second.y)>50)){
+                continue;
+            }
+            rectangle(garb_scene, matchPoint->second, Point( matchPoint->second.x + num.cols , matchPoint->second.y + num.rows ), Scalar::all(0), CV_FILLED, 8, 0 );
+            //imshow("big " + to_string(i) + " " + to_string(matchPoint->first),garb_scene);
+            //waitKey(0);
+            if(yLoc == 0){
+                yLoc = matchPoint->second.y;
+            }
+            numbers.push_back(*new Point(i,matchPoint->second.x));
+            if(numbers.size() == 6){
+                i = 10;
+                continue;
+            }
+            i = -1;
+            continue;
+        }
+    }
+    
+    numbers.sort(compPoints);
+    
+    list<Point>::iterator iter;
+    int milliseconds = 0;
+    int position = 0;
+    for (iter = numbers.begin() ; iter != numbers.end(); ++iter){
+        switch(position){
+            case 0:
+                milliseconds += iter->x * 600000;
+                position++;
+                break;
+            case 1:
+                milliseconds += iter->x * 60000;
+                position++;
+                break;
+            case 2:
+                milliseconds += iter->x * 10000;
+                position++;
+                break;
+            case 3:
+                milliseconds += iter->x * 1000;
+                position++;
+                break;
+            case 4:
+                milliseconds += iter->x * 100;
+                position++;
+                break;
+            case 5:
+                milliseconds += iter->x * 10;
+                position++;
+                break;
+        }
+    }
+
+    return 480000-milliseconds;
+}
+
+bool compPoints (Point i,Point j) { return (i.y<j.y); }
+
+void setUpNumbers(cv::Mat scene)
+{
+    getScale(scene);
+    
+    cout << videoScale << endl;
+    
+    Zero = imread("/Users/peterdavids/Desktop/Projects/smashProj/smashProj/Resources/zero.png",1);
+    Size size = Zero.size();
+    resize(Zero,Zero,cvSize(size.width * videoScale, size.height * videoScale));
+    
+    One = imread("/Users/peterdavids/Desktop/Projects/smashProj/smashProj/Resources/one.png",1);
+    size = One.size();
+    resize(One,One,cvSize(size.width * videoScale, size.height * videoScale));
+    
+    Two = imread("/Users/peterdavids/Desktop/Projects/smashProj/smashProj/Resources/two.png",1);
+    size = Two.size();
+    resize(Two,Two,cvSize(size.width * videoScale, size.height * videoScale));
+    
+    Three = imread("/Users/peterdavids/Desktop/Projects/smashProj/smashProj/Resources/three.png",1);
+    size = Three.size();
+    resize(Three,Three,cvSize(size.width * videoScale, size.height * videoScale));
+    
+    Four = imread("/Users/peterdavids/Desktop/Projects/smashProj/smashProj/Resources/four.png",1);
+    size = Four.size();
+    resize(Four,Four,cvSize(size.width * videoScale, size.height * videoScale));
+    
+    Five = imread("/Users/peterdavids/Desktop/Projects/smashProj/smashProj/Resources/five.png",1);
+    size = Five.size();
+    resize(Five,Five,cvSize(size.width * videoScale, size.height * videoScale));
+    
+    Six = imread("/Users/peterdavids/Desktop/Projects/smashProj/smashProj/Resources/six.png",1);
+    size = Six.size();
+    resize(Six,Six,cvSize(size.width * videoScale, size.height * videoScale));
+    
+    Seven = imread("/Users/peterdavids/Desktop/Projects/smashProj/smashProj/Resources/seven.png",1);
+    size = Seven.size();
+    resize(Seven,Seven,cvSize(size.width * videoScale, size.height * videoScale));
+    
+    Eight = imread("/Users/peterdavids/Desktop/Projects/smashProj/smashProj/Resources/eight.png",1);
+    size = Eight.size();
+    resize(Eight,Eight,cvSize(size.width * videoScale, size.height * videoScale));
+    
+    Nine = imread("/Users/peterdavids/Desktop/Projects/smashProj/smashProj/Resources/nine.png",1);
+    size = Nine.size();
+    resize(Nine,Nine,cvSize(size.width * videoScale, size.height * videoScale));
+=======
     return bestScale;
+>>>>>>> a841d0afdc6bacf5d2ed2eec03b8e8f750421b3b
 }
 
 
